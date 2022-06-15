5df596dcea4873c7666a77244ff9d7b87a3031e9
Silver_Hsiao <silver041692@gmail.com>
2022-06-15 23:12:57 +0800

    test

--- app/src/main/java/com/example/iotapp/MainActivity.kt
+++ app/src/main/java/com/example/iotapp/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val a:Int = 1
-
+        val b:Int = 2
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
