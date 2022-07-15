fca3c94f54db61f770b0c83bab939b42854b0445
weidot <walton6180831@gmail.com>
2022-07-15 16:47:43 +0800

    修改底部導覽列監聽事件並調整平板未登入介面

--- app/src/main/java/com/example/iotapp/MainActivity.kt
+++ app/src/main/java/com/example/iotapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.iotapp
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -14,12 +13,14 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import com.example.iotapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private  var navController:NavController? = null
+    private var navController: NavController? = null
+    private var title: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +30,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         toolbar.inflateMenu(R.menu.menu_main)
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = getString(R.string.fragment_home)
+        title = findViewById(R.id.toolbar_title)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_Layout)
         val toggle = ActionBarDrawerToggle(
             this,
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navController = findNavController(R.id.nav_host_fragment_content_main)
-
+        onNavigationItemClick()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -51,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    //控制工具列右邊圖示的點擊
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -67,32 +68,36 @@ class MainActivity : AppCompatActivity() {
                 || super.onSupportNavigateUp()
     }
 
-    fun onHomeClick(item: MenuItem) {
-        navController?.navigate(R.id.FirstFragment)
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = getString(R.string.fragment_home)
-    }
+    //控制底下导航栏的点击事件
+    private fun onNavigationItemClick() {
 
-    fun onFamilyClick(item: MenuItem) {
-        navController?.navigate(R.id.SecondFragment)
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = getString(R.string.fragment_family)
-    }
-
-    fun onClickLogin(view: View) {
-        navController?.navigate(R.id.SecondFragment)
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = getString(R.string.fragment_family)
-    }
-
-    fun onModeClick(item: MenuItem) {
-        navController?.navigate(R.id.ThirdFragment)
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = getString(R.string.fragment_mode)
-    }
-    fun onLogClick(item: MenuItem) {
-        navController?.navigate(R.id.FourthFragment)
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = getString(R.string.fragment_log)
+        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_settings -> {
+                    title?.text = getString(R.string.fragment_home)
+                    navController?.navigate(R.id.FirstFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.action_manage -> {
+                    title?.text = getString(R.string.fragment_family)
+                    navController?.navigate(R.id.SecondFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.action_mode -> {
+                    title?.text = getString(R.string.fragment_mode)
+                    navController?.navigate(R.id.ThirdFragment)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.action_log -> {
+                    title?.text = getString(R.string.fragment_log)
+                    navController?.navigate(R.id.FourthFragment)
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    super.onOptionsItemSelected(item)
+                }
+            }
+        }
     }
 }
