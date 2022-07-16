42f98b174fb22a86a8e79ddd2aee2ce67c41fe2e
wei-dot <walton6180831@gmail.com>
2022-07-16 22:22:41 +0800

    修復標題無法置中問題

--- app/src/main/java/com/example/iotapp/MainActivity.kt
+++ app/src/main/java/com/example/iotapp/MainActivity.kt
@@ -2,18 +2,18 @@ package com.example.iotapp
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.example.iotapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -21,7 +21,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var title: TextView? = null
     private lateinit var appBarConfiguration: AppBarConfiguration
 
 
@@ -30,18 +29,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+
         val navView: BottomNavigationView = binding.appBarMain.bottomNavigation
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
-
-
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+//        監聽nav目的地變化後修改標題
+        val title: TextView = findViewById(R.id.toolbar_title)
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            title.text = navController.currentDestination?.label
+        }
         toolbar.inflateMenu(R.menu.bottom_nav_menu)
 
-        val drawerLayout: DrawerLayout = binding.drawerLayout
         val toggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
